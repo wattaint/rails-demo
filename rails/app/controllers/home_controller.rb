ad6471ea88cd75a74107a809cf93b9200450f29b
wattana.int <wattana.int@ascendcorp.com>
2019-03-23 15:16:02 +0700

    test pretty json render

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
+require 'redcarpet'
+require 'rouge'
+require 'rouge/plugins/redcarpet'
+
 class HomeController < ApplicationController
+  include Rouge::Plugins::Redcarpet
+  
   def index
-    render json: { message: 'Rails Demo', env: ENV }
+    #puts JSON.pretty_generate({ message: 'Rails Demo', env: ENV })
+    #render text: JSON.pretty_generate({ message: 'Rails Demo', env: ENV })
+   
+    obj = { message: 'Rails Demo', env: ENV.to_hash }
+    render json: JSON.pretty_generate(obj.as_json)
   end
 end
